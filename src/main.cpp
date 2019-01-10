26c1619d6f744fcc1db299c0bc809ae03ce738cc
Daniel Christiansen <christiansen.daniel@gmail.com>
2019-01-10 13:19:22 -0800

    fixed ftm0 timer initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,13 @@
  * TODO:
  *
  * Functionality:
- * - Port to teensy 3.6
- *   x Set up DAC
- *   x Set up DMA transfer to DAC
- *   x Set up multiple-buffer use
- *   - Set up state-update timer
  * - Improve state update algorithm 
  * - Add input to excite system
  * - Add add parameter control pots
  *
  * Structure:
  * - Abstract from hardware slightly (module interfaces)
+ * - Split DAC setup into DMAMUX/eDMA & DAC/PDB setups
  *
  */
 
@@ -28,7 +24,7 @@
 #include "system.h"
 #include "arm_math.h"
 
-void tpm_setup();
+void tpm_setup(uint8_t, uint16_t);
 
 volatile uint16_t output_buffer[OUTPUT_BUFFER_SIZE] __attribute__((aligned (OUTPUT_BUFFER_SIZE*2)));
 volatile uint16_t* output_buffer_0 = output_buffer;
@@ -44,9 +40,9 @@ volatile uint8_t state_flag = 0;
 
 volatile uint8_t led_val = 0;
 
-volatile uint8_t int_count = 0;
+volatile uint16_t led_count = 0;
 
-uint16_t F_UPDATE = 100;
+uint16_t F_UPDATE = 1;
 uint16_t UPDATE_PS = 16;
 uint16_t UPDATE_TIMER = F_BUS / (F_UPDATE * UPDATE_PS);
 
@@ -57,7 +53,7 @@ extern "C" int main(void) {
 	Oscillator osc{};
 	
 	// phase_step = audio frequency / sample frequency
-	osc.phase_step = (440.0/(float)F_SAMPLE);
+	osc.phase_step = (60.0/(float)F_SAMPLE);
 	//osc.phase_step = (1.0 / (float)OUTPUT_BUFFER_SIZE);
 
 	// Fill sample buffers
@@ -74,55 +70,62 @@ extern "C" int main(void) {
 
 	dac0.Init(output_buffer_0);
 	
-	//tpm_setup();
+	tpm_setup(UPDATE_PS, UPDATE_TIMER);
 
-
+	//digitalWrite(13, HIGH);	
+	
 	// Main task loop
 	while(1) {
 		// Sample buffer population
 		if (buffer_flag) {
 			if (buffer_tracker) {
-				massSystem.generate_table(output_buffer_0, OUTPUT_BUFFER_SIZE/2, osc.phase_step, &osc.phase_accumulator);
+				massSystem.generate_table(output_buffer_0, OUTPUT_BUFFER_SIZE/2, 
+						osc.phase_step, &osc.phase_accumulator);
 				buffer_0_ready = 1;
 			} else {
-				massSystem.generate_table(output_buffer_1, OUTPUT_BUFFER_SIZE/2, osc.phase_step, &osc.phase_accumulator);
+				massSystem.generate_table(output_buffer_1, OUTPUT_BUFFER_SIZE/2, 
+						osc.phase_step, &osc.phase_accumulator);
 				buffer_1_ready = 1;
 			}
 			buffer_flag = 0;
 		}
 		
 		// Mass system state update
-		else if (state_flag) {
+		if (state_flag) {
 			massSystem.update_state(0.01);
 			state_flag = 0;
+			digitalWrite(13, HIGH);
 		}
 	}
 
 }
 
 
-void tpm_setup() {
-	FTM0_SC = 0;	// Disable TPM0
-	FTM0_CNT = 0;	// Clear counter
-	//FTM0_MOD = 0x3A98; // Set mod to 15,000
-	FTM0_MOD = (0x3A98) >> 2;
+void tpm_setup(uint8_t prescaler, uint16_t update_timer) {
 
+	SIM_SCGC6 |= SIM_SCGC6_FTM0; // Enable FTM0 clock
+	
+	NVIC_ENABLE_IRQ(IRQ_FTM0);
+
+	FTM0_MODE |= FTM_MODE_WPDIS; // Disable write protection
+	FTM0_SC = 0x0;	// Disable TPM0
+	FTM0_CNT = 0x0;	// Clear counter
+	FTM0_MOD = (update_timer);
+
+	
 	FTM0_SC |=
 		(FTM_SC_TOF			// Clear overflow flag
 		|FTM_SC_TOIE		// Enable overflow interrupt
 		|FTM_SC_CLKS(1)	// Enable timer, count on internal clock
-		|FTM_SC_PS(4));	// Set timer prescaler to 16	
+		|FTM_SC_PS(__builtin_clz(prescaler)));	
 	
 	// Enable PTM interrupts
-	NVIC_SET_PRIORITY(IRQ_FTM0, 64);
-	NVIC_ENABLE_IRQ(IRQ_FTM0);
+	//NVIC_CLEAR_PENDING(IRQ_FTM0);
 }
 
 // Interrupt routine to set next sample buffer as DMA source
 void dma_ch0_isr(void) {
 	DMA_CINT = 0; // Clear interrupt bit
-
-	int_count++;
 
 	if (buffer_tracker && buffer_0_ready) {
 		buffer_0_ready = 0;
@@ -133,16 +136,15 @@ void dma_ch0_isr(void) {
 		//dac0.TCD.SADDR = (volatile const uint16_t *)output_buffer_1;
 		buffer_tracker = 1;
 	} else {
-		digitalWrite(13, HIGH);
+		//digitalWrite(13, HIGH);
 	}
 	buffer_flag = 1;
-	//digitalWrite(13, HIGH);
-	digitalWrite(13, (int_count & 0x80) ? HIGH : LOW);
+
 }
 
 // Interrupt routine to time state updates
 void ftm0_isr(void) {
-	FTM0_SC |= FTM_SC_TOF;
-	
+	FTM0_SC &= ~FTM_SC_TOF;
 	state_flag = 1;
+	digitalWrite(13, HIGH);
 }	
